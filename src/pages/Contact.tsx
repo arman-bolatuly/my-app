7f9faaf62bdf0b7e002104cd18fc9bd14fe5a38b
Arman Bulatuly <arman_bulatovich@mail.ru>
2022-09-14 20:07:44 +0600

    12

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -108,36 +108,6 @@ export default function Contact() {
                       </Button>
                     </VStack>
                   </Box>
-                  {/* <HStack
-                    mt={{ lg: 10, md: 10 }}
-                    spacing={5}
-                    px={5}
-                    alignItems="flex-start">
-                    <IconButton
-                      aria-label="facebook"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      // icon={<MdFacebook size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="github"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      //icon={<BsGithub size="28px" />}
-                    />
-                    <IconButton
-                      aria-label="discord"
-                      variant="ghost"
-                      size="lg"
-                      isRound={true}
-                      _hover={{ bg: '#0D74FF' }}
-                      //icon={<BsDiscord size="28px" />}
-                    />
-                  </HStack> */}
                 </Box>
               </WrapItem>
               <WrapItem>
